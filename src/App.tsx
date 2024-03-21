16988c9ca20b7e9b265b98db10b3cdc8d6f1b3c6
Matthew Beale <129032908+mttble@users.noreply.github.com>
2024-03-21 11:47:26 +1030

    basic form using react-hook-form and some basic validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-
+import {SubmitHandler, useForm} from 'react-hook-form'
 import './App.css'
 
-function App() {
+type FormFields = {
+  email: string
+  password: string
+}
 
+const App = () =>{
+  
+  const {register, handleSubmit } = useForm<FormFields>()
+
+  const onSubmit: SubmitHandler<FormFields> = (data) => {
+    console.log(data)
+  }
 
   return (
-    <div>Hello World!</div>
+    <form className='tutorial gap-2' onSubmit={handleSubmit(onSubmit)}>
+
+      <div>
+        <input {...register("email", {
+          required:true,
+          pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+          })} type='text' placeholder='Email' />
+      </div>
+
+      <div>
+        <input {...register("password", {
+          required:true,
+          minLength: 8,
+          })} type='password' placeholder='Password' />
+      </div>
+
+      <div>
+        <button type='submit'>Submit</button>
+      </div>
+
+    </form>
   )
 }
 
